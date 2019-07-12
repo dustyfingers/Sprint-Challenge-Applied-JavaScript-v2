27b318108e9b5094d92e4cc584d944e227fd1ccd
dustyfingers <dustyfingers01257@gmail.com>
2019-07-12 13:13:25 -0600

    finished tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,3 +1,12 @@
+function createDOMElement(type, classes = '', content = '') {
+    if (!type) return;
+    let DOMElement = document.createElement(type);
+    if (classes) DOMElement.classList.add(classes);
+    DOMElement.textContent = content;
+    return DOMElement;
+}
+
+
 // Step 2: Create Tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
@@ -7,3 +16,19 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get
+
+function Tab(topic) {
+    let tabDiv = createDOMElement('div', 'tab', topic);
+    return tabDiv;
+}
+
+async function buildTabs() {
+    let tabsContainer = document.querySelector('.topics');
+    let topics = await axios.get(`https://lambda-times-backend.herokuapp.com/topics`);
+    topicsArray = topics.data.topics;
+    topicsArray.forEach(topic => tabsContainer.appendChild(Tab(topic)));
+};
+
+buildTabs();
